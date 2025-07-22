269f51b7fa2c2945a348bd385d325efd8955ec92
Luxz - Official <luzfimax@gmail.com>
2025-07-22 08:16:23 +0700

    Add files via upload

---
+++ Tmp/tmp.js
@@ -0,0 +1,15 @@
+/* 
+
+   ####### Selfbot Version 2 ########
+   
+   Developer: 
+   - Skyzopedia (+6283181145670)
+   
+   Recode:
+   - Isi namamu (sosmed)
+   
+   # Penting
+   Jangan hapus credits atau nama developer
+   hargai pembuat script ini!
+
+*/
